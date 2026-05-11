2a6e37fe6089091271b000c2690344afa49a8c6d
Valeriia Motas <a240248vm@rvt.lv>
2026-05-11 05:41:02 -0700

    SQLite

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,8 +1,19 @@
 package rvt;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class App {
     public static void main(String[] args) {
-        // Your code goes here...
-        System.out.println("Test");
+            try {
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:data.db");
+        Statement stmt = conn.createStatement();
+
+        stmt.executeUpdate("create table todo (id integer, task string)");
+        } catch (SQLException e) {
+
+}
     }
 }
